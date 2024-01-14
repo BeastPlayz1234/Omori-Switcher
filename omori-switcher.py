742073e55ed7203e84e81f115a35f820993ddef5
BeastPlayz1234 <beastplayz1234yt@gmail.com>
2024-01-14 16:49:43 -0500

    Fixed the wording of some lines

--- omori-switcher.py
+++ omori-switcher.py
@@ -12,18 +12,18 @@ def main():
         # Creates file to write to
         f = open(os.path.dirname(os.path.realpath(__file__)) + '\\steam.txt', 'w')
         # Asks for the steam path
-        location = input("Enter your steam path: ")
+        location = input("Enter your steam game folder path: ")
         f.write(location)
         # This flush line shouldn't be neccesarry cause closing the program SHOULD do this but it never does for some reason; i should find a better way to do this
         f.flush()
         f.close()
         check = 'fugfdgjg'
         while check != 'Y' and check != 'y' and check != 'n' and check != 'N':
-            check = input("Is your game currently modded:")
+            check = input("Is your game currently modded (Y/N): ")
         if check == 'Y' or check == 'y':
             try:
                 os.rename(location + '\\Omori', location + "\\Omori (Modded)")
-                print("Succefully renamed folder. Now reinstall Omori through Steam, then the script should auto switch your game version between modded and unmodded. Press any key to exit script")
+                print("Succefully renamed folder. Now reinstall Omori through Steam, then the script should auto switch your game version between modded and unmodded. Press enter to exit script")
                 no = input("")
                 sys.exit()
             except OSError:
@@ -33,8 +33,8 @@ def main():
         else:
             try:
                 os.rename(location + 'Omori', location + '\\Omori (Vanilla)')
-                print("Succefully renamed folder. Now reinstall Omori through Steam, mod the game, then the script should auto switch your game between modded and unmodded")
-                input()
+                print("Succefully renamed folder. Now reinstall Omori through Steam, mod the game, then the script should auto switch your game between modded and unmodded. Press enter to exit script")
+                no = input("")
                 sys.exit()
             except OSError:
                 print("An error occured. This is usually caused by having the game directory open in file explorer while running the script. Please close the file explorer, then run the script again.")
