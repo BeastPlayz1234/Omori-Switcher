e6d9df8e33ad12520700eaace12c082b7ff09157
BeastPlayz1234 <beastplayz1234yt@gmail.com>
2024-01-18 19:06:40 -0500

    Update omori-switcher.py

--- omori-switcher.py
+++ omori-switcher.py
@@ -3,26 +3,26 @@ import os, sys # For file control and exception catching
 def main():
     try:
         # Attempts to open config file for script
-        f = open(os.path.dirname(os.path.realpath(__file__)) + '\\steam.txt', 'r')
+        f = open(os.path.dirname(os.path.realpath(__file__)) + "\\steam.txt", 'r')
         steam = f.readline()
         f.close()
         sys.exit("Script succeccful, your game should now be your " + rename_game(steam) + " install")
     # If the file didn't exist, run these lines
     except FileNotFoundError:
         # Creates file to write to
-        f = open(os.path.dirname(os.path.realpath(__file__)) + '\\steam.txt', 'w')
+        f = open(os.path.dirname(os.path.realpath(__file__)) + "\\steam.txt", 'w')
         # Asks for the steam path
         location = input("Enter your steam game folder path: ")
         f.write(location)
         # This flush line shouldn't be neccesarry cause closing the program SHOULD do this but it never does for some reason; i should find a better way to do this
         f.flush()
         f.close()
-        check = 'fugfdgjg'
+        check = "fugfdgjg"
         while check != 'Y' and check != 'y' and check != 'n' and check != 'N':
             check = input("Is your game currently modded (Y/N): ")
         if check == 'Y' or check == 'y':
             try:
-                os.rename(location + '\\Omori', location + "\\Omori (Modded)")
+                os.rename(location + "\\Omori", location + "\\Omori (Modded)")
                 print("Succefully renamed folder. Now reinstall Omori through Steam, then the script should auto switch your game version between modded and unmodded. Press enter to exit script")
                 no = input("")
                 sys.exit()
@@ -32,7 +32,7 @@ def main():
                 sys.exit()
         else:
             try:
-                os.rename(location + 'Omori', location + '\\Omori (Vanilla)')
+                os.rename(location + "Omori", location + "\\Omori (Vanilla)")
                 print("Succefully renamed folder. Now reinstall Omori through Steam, mod the game, then the script should auto switch your game between modded and unmodded. Press enter to exit script")
                 no = input("")
                 sys.exit()
